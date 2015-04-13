65e46452976057e425a2805ab6600719a4fa78ea
Nan Wang <nantas@gmail.com>
2015-04-13 11:26:05 +0800

    increase memory limit for account-server.

--- gulpfile.js
+++ gulpfile.js
@@ -100,7 +100,7 @@ gulp.task('stop', function() {
 });
 
 gulp.task('run', function(cb) {
-  var child = spawn('pm2', ['start', 'server/server.js', '--name', 'account-server', '--max-memory-restart', '80M'], {
+  var child = spawn('pm2', ['start', 'server/server.js', '--name', 'account-server', '--max-memory-restart', '150M'], {
     cwd: destPath
   });
   child.on('exit', function() {
